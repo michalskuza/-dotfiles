eac969d7112d92d8b89fbb73439be30fde494fa0
Michal Skuza <michal@michalskuza.com>
2015-05-19 22:44:40 +0200

    Added config.fish

---
+++ config.fish
@@ -0,0 +1,25 @@
+# some more ls aliases
+alias ls='ls --color'
+alias ll='ls -lFh'
+alias la='ls -Al'
+alias l='ls -CF'
+alias li='ls -1'
+
+alias less='less -R'
+alias duh='du -h -d 1'
+alias untar='tar xvfz'
+
+#using GNU grep
+alias grep='ggrep --color=always'
+alias fgrep='ggrep -F --color=always'
+alias egrep='ggrep -E --color=always'
+
+alias find='gfind'
+alias locate='glocate'
+alias updatedb='gupdatedb'
+alias xargs='gxargs'
+alias g='git'
+alias j='java'
+alias jc='javac'
+alias jp='javap'
+alias jpv='javap -verbose'
